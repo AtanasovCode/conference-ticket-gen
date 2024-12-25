c068ae82f72046285c63e9665981bdb72268b6f2
AtanasovCode <a_ljupce@yahoo.com>
2024-12-25 17:20:32 +0100

    created Inputs component with reusable input components

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,15 @@
+import PhotoInput from "./PhotoInput";
+import TextInput from "./TextInput";
+
+const Inputs = () => {
+    return (
+        <div>
+            <PhotoInput />
+            <TextInput name="" updateValue={} />
+            <TextInput name="" updateValue={} />
+            <TextInput name="" updateValue={} />
+        </div>
+    );
+}
+
+export default Inputs;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,10 +1,14 @@
 
+
+// Components
 import Hero from "../components/Hero";
+import Inputs from "../components/Inputs";
 
 const Home = () => {
     return (
-        <div className="h-dvh bg-neutral-900 py-12 flex flex-col items-center justify-start text-neutral-0">
+        <div className="h-dvh bg-neutral-900 py-12 flex flex-col items-center justify-start text-neutral-0 gap-16">
             <Hero />
+            <Inputs />
         </div>
     );
 }
